b846399cdb5100d34a62d4dc002333c60ed9d233
Leonardo Reyes <Leonardo.ReyesRivero@wnco.com>
2019-12-16 13:52:27 -0500

    Customize exceptions and internationalize messages

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloword/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloword/HelloWorldController.java
@@ -1,25 +1,45 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloword;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
+
 	@GetMapping(path = "hello-world")
 	public String helloWorld() {
 		return "Hello world";
 	}
-	
+
 	@GetMapping(path = "hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello world");
 	}
-	
+
 	@GetMapping(path = "hello-world-bean/path-variable/{name}")
 	public HelloWorldBean helloWorldBean(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello world, %s", name));
 	}
+
+	@GetMapping(path = "hello-world-internationalized")
+	public String helloWorldInternationalized(
+			@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+		return messageSource.getMessage("good.morning.message", null, locale);
+	}
+	
+//	@GetMapping(path = "hello-world-internationalized")
+//	public String helloWorldInternationalized() {
+//		return "Good morning";
+//	}
 
 }
--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -3,8 +3,13 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,24 +28,44 @@ public class UserResource {
 		return service.findAll();
 	}
 
+//	@GetMapping(path = "/users/{id}")
+//	public User retrieveUser(@PathVariable int id) {
+//		User user = service.findOne(id);
+//		if(user == null) {
+//			throw new UserNotFoundException("id-"+id);
+//		}
+//		return user;
+//	}
+
 	@GetMapping(path = "/users/{id}")
-	public User retrieveUser(@PathVariable int id) {
+	public Resource<User> retrieveUser(@PathVariable int id) {
 		User user = service.findOne(id);
-		if(user == null) {
-			throw new UserNotFoundException("id-"+id);
+		if (user == null) {
+			throw new UserNotFoundException("id-" + id);
 		}
-		return user;
+
+		Resource<User> resource = new Resource<User>(user);
+		ControllerLinkBuilder linkTo = ControllerLinkBuilder
+				.linkTo(ControllerLinkBuilder.methodOn(this.getClass()).retrieveAllUsers());
+		resource.add(linkTo.withRel("all-users"));
+		return resource;
+	}
+
+	@DeleteMapping(path = "/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		User user = service.deleteById(id);
+		if (user == null) {
+			throw new UserNotFoundException("id-" + id);
+		}
 	}
 
 	@PostMapping(path = "/users")
-	public ResponseEntity<Object> createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
 		User savedUser = service.save(user);
 		// Redirect to the user created
-		URI location = ServletUriComponentsBuilder
-			.fromCurrentRequest()
-			.path("/{id}")
-			.buildAndExpand(savedUser.getId()).toUri();
-		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
+				.toUri();
+
 		return ResponseEntity.created(location).build();
 	}
 
