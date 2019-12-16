55b60419a0ac3b6bc853b49cbeedebc80350e1e1
Leonardo Reyes <leonardo.reyes@colfuturo.org>
2019-12-16 05:45:07 -0500

    first commit

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,36 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+	
+	@Autowired
+	private UserDaoService service;
+	
+	@GetMapping(path="/users")
+	public List<User> retrieveAllUsers(){
+		return service.findAll();
+	}
+	
+	@GetMapping(path="/users/{id}")
+	public User retrieveUser(@PathVariable int id){
+		return service.findOne(id);
+	}
+	
+	@PostMapping(path="/users")
+	public void createUser(@RequestBody User user){
+		User userCreated = service.save(user);
+		if(userCreated.getId()!=null){
+			System.out.println(userCreated);
+		}
+	}
+
+}
